6c05bf6ac19bdd36a950cc6fbe7f6093f77b01ab
wispwisp <wisp@gcnet.ru>
2015-11-23 15:39:03 +0300

    todo correct insertion sort

--- algorithms/Sorts/2insertion/insertion.cpp
+++ algorithms/Sorts/2insertion/insertion.cpp
@@ -10,21 +10,7 @@ template <typename Cont> Cont sorted(Cont&& c) {
   for (auto outer_it = std::begin(c);
        outer_it != std::end(c);
        ++outer_it) {
-    
-    auto elem = *outer_it;
-    auto inner_it = outer_it;
-    for(;inner_it != std::begin(c);) {
-      if (*(--inner_it) < elem) {
-	inner_it++;
-	break;
-      }
-      else
-	inner_it++;
-      auto temp_it = inner_it;
-      *temp_it = *(--inner_it);
-    }
-
-    *inner_it = elem;
+    ; // todo
   }
 
   return c;
@@ -35,7 +21,7 @@ int main () {
   std::vector<int> vec = { 18,0,1,12,14,8,19,2,7 };
   vec = sorted(std::move(vec));
   
-  cout << "sorted mas:\n";
+  cout << "sorted: ";
   std::ostream_iterator<int, char> outIter(std::cout, ", ");
   std::copy(std::begin(vec), std::end(vec), outIter);
   cout << endl;
