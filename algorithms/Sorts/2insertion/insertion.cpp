1902db9e072cf76b6536509ca1d07f05fc602c60
wispwisp <wisp@gcnet.ru>
2015-11-23 13:26:10 +0300

    insertion sort: refac., ADL swap

--- algorithms/Sorts/2insertion/insertion.cpp
+++ algorithms/Sorts/2insertion/insertion.cpp
@@ -6,14 +6,14 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-template <typename T>
-T sorted(T&& mas) {
-  for (auto outer_it = std::begin(mas);
-       outer_it != std::end(mas);
+template <typename Cont> Cont sorted(Cont&& c) {
+  for (auto outer_it = std::begin(c);
+       outer_it != std::end(c);
        ++outer_it) {
+    
     auto elem = *outer_it;
     auto inner_it = outer_it;
-    for(;inner_it != std::begin(mas);) {
+    for(;inner_it != std::begin(c);) {
       if (*(--inner_it) < elem) {
 	inner_it++;
 	break;
@@ -23,20 +23,21 @@ T sorted(T&& mas) {
       auto temp_it = inner_it;
       *temp_it = *(--inner_it);
     }
+
     *inner_it = elem;
   }
 
-  return mas;
+  return c;
 }
 
 int main () {
 
-  std::vector<int> mas = { 18,0,1,12,14,8,19,2,7 };
-  mas = sorted(std::move(mas));
+  std::vector<int> vec = { 18,0,1,12,14,8,19,2,7 };
+  vec = sorted(std::move(vec));
   
-  cout << "sorted mas: " << endl;
+  cout << "sorted mas:\n";
   std::ostream_iterator<int, char> outIter(std::cout, ", ");
-  std::copy(std::begin(mas), std::end(mas), outIter);
+  std::copy(std::begin(vec), std::end(vec), outIter);
   cout << endl;
 
   return 0;
