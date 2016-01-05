f7829f2d27a9564f4b1c59140be3e45ccdba88cd
wispwisp <wisp@gcnet.ru>
2016-01-05 17:13:09 +0300

    refactoring, correctness

--- algorithms/Sorts/quickSort/quick.cpp
+++ algorithms/Sorts/quickSort/quick.cpp
@@ -9,12 +9,12 @@ using std::endl;
 template<typename Iter> Iter partition(Iter l, Iter r) {
 
   using std::swap;
-  Iter i = l, j = std::prev(r), v = r;
+  Iter i = l, j = std::prev(r);
   while(true) {
-    while (*i < *v) ++i; // i`s <= central element
-    while (*v < *j) { // j`s >= central element
-      --j;
+    while (*i < *r) ++i; // i`s <= central element
+    while (*r < *j) { // j`s >= central element
       if (j == l) break;
+      --j;
     }
 
     if (i >= j) break;
@@ -35,8 +35,9 @@ template<typename Iter> void qsorted(Iter l, Iter r) {
   qsorted(std::next(i), r);
 }
 
-template<typename Cont> Cont sorted(Cont&& c) {
-  qsorted(std::begin(c), std::prev(std::end(c)));
+template<typename Cont> Cont sorted(Cont c) {
+  if (!c.empty())
+    qsorted(std::begin(c), std::prev(std::end(c)));
   return c;
 }
 
